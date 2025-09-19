86cdce8b7bccb55534dab441056dd0e82c08de68
mohzak1 <mohzak@uchicago.edu>
2025-09-19 13:37:40 -0500

    Create total_return.py

---
+++ total_return.py
@@ -0,0 +1,6 @@
+def realized_return(last_value: float, first_value: float): 
+    
+    
+    realized_return = ((last_value-first_value)/(first_value))
+    
+    print(F"The realized return is {realized_return} in totality")
