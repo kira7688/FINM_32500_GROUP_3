a1923dba955aae9c896d4a4360f7a602f9570ec1
mohzak1 <mohzak@uchicago.edu>
2025-09-19 13:38:24 -0500

    max_drawdown.py

---
+++ max_drawdown.py
@@ -0,0 +1,22 @@
+def drawdown: 
+    drawdowns = []
+    prices = []
+    peak = price[0]
+    
+    max_drawdown = 0
+    
+    for p in prices: 
+        
+        if p > peak: 
+            p = peak 
+            
+    drawdown = ((peak-p)/peak)
+    drawdowns.append(drawdown)
+    
+    if drawdown > max_drawdown: 
+        
+        max_drawdown = drawdown
+        
+        
+    Print(F"The current maximum drawdown is {max_drawdown}"
+    Print(F"The current drawdown indexed by a single unit of time is {drawdowns}"
